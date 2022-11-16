8a23b2337b95de663bce951d7142b2f769b7ff6e
khadiza1209 <116022910+khadiza1209@users.noreply.github.com>
2022-11-16 12:15:11 +0600

    Add files via upload

---
+++ class#17 hw/js/script.js
@@ -0,0 +1,11 @@
+$('.sliders').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:true,
+    prevArrow:'<i class="fa-solid fa-circle-chevron-left left_one"></i>',
+    nextArrow:'<i class="fa-solid fa-circle-chevron-right right_one"></i>',
+    dots:true,
+  });
+      
